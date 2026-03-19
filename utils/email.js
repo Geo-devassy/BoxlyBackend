ed7fb543bb5f19ae25e615e71bd29ae0e12a0f8d
Geo Devassy <gdevassy17@gmail.com>
2026-03-19 15:47:46 +0530

    fix: switch to STARTTLS port 587 for better cloud SMTP reliability

--- utils/email.js
+++ utils/email.js
@@ -1,10 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false, // use false for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
